bf8c4cb1a055c58964a505fb64c4d9680f3a3b92
Laurent Mbuyu <laurentm@osiristrading.com>
2024-03-18 11:15:32 +0200

    [New]: Winning and losing component has been added.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -12,7 +12,7 @@ function GuessInput({ answer }) {
     console.info({ guess: guessInUppercase });
 
     const nextGuess = [...guesses, guess];
-    setGuesses(nextGuess)
+    setGuesses(nextGuess);
   }
 
   return <>
@@ -24,6 +24,22 @@ function GuessInput({ answer }) {
         setGuess(event.target.value);
       }} />
     </form>
+
+    {
+      guess === answer && <div class="happy banner">
+        <p>
+          <strong>Congratulations!</strong> Got it in
+          <strong>3 guesses</strong>.
+        </p>
+      </div>
+    }
+
+    {
+      guesses.length > 4 && <div class="sad banner">
+        <p>Sorry, the correct answer is <strong>LEARN</strong>.</p>
+      </div>
+    }
+
   </>;
 }
 
